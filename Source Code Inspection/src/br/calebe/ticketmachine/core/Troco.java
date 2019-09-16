15fe515e775dea892facad9979dd1be99d8b6f28
André Oliveira <castrodeko11@hotmail.com>
2019-09-16 00:01:15 -0300

    Troco variavel sempre zerada, correção

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -11,28 +11,28 @@ class Troco {
     protected PapelMoeda[] papeisMoeda;
 
     public Troco(int valor) {
-        papeisMoeda = new PapelMoeda[6];
+        papeisMoeda = new PapelMoeda[5];
         int count = 0;
         while (valor % 100 != 0) {
             count++;
         }
         papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
+
         while (valor % 50 != 0) {
             count++;
         }
         papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
+
         while (valor % 20 != 0) {
             count++;
         }
         papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
+
         while (valor % 10 != 0) {
             count++;
         }
         papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
+
         while (valor % 5 != 0) {
             count++;
         }
